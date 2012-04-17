34223430e78b0311a289a08cf284b8121487df89
Olenka.Joerg@gmail.com <Olenka.Joerg@gmail.com@4efd2022-0513-11df-958a-03df8639dc2f>
2012-04-17 07:27:56 +0000

    add official name into app bundle

--- create_mac_bundle.sh
+++ create_mac_bundle.sh
@@ -2,10 +2,33 @@
 APPNAME=${1}
 TMPFILE=/tmp/plist.tmp
 CONTENTS=$APPNAME.app/Contents
+OFFNAME=""
 
+# create official name ...
+case $APPNAME in
+    kartina_tv)
+        OFFNAME="Kartina.TV"
+        ;;
+    polsky_tv)
+        OFFNAME="Polsky.TV"
+        ;;
+    vlc-record)
+        OFFNAME="VLC-Record"
+        ;;
+    afrobox)
+        OFFNAME="AfroBox"
+        ;;
+    *)
+        OFFNAME=$APPNAME
+        ;;
+esac
+
+# get version info from version_info.h header ...
 MINORVER=`sed -n 's/^#define[ \t]*VERSION_MINOR[^0-9]*\([^"]*\).*/\1/p' version_info.h`
 BETAEXT=`sed -n 's/^#define[ \t]*BETA_EXT[^0-9B]*\([^"]*\).*/\1/p' version_info.h`
 DATESTR=`date +%Y%m%d`
+
+# create destination folder name ...
 DSTFOLDER=$APPNAME-2.${MINORVER}${BETAEXT}-${DATESTR}-mac
 cd release
 rm -rf $APPNAME-*-mac*
@@ -16,11 +39,11 @@ cp ../*.qm $CONTENTS/Resources/language/
 cp ../modules/*.mod $CONTENTS/PlugIns/modules/
 cp ../resources/$APPNAME.icns $CONTENTS/Resources/$APPNAME.icns
 cp -R ../mac/* $CONTENTS/MacOS
-# POS=`pwd`
-# cd $CONTENTS/MacOS
-# ln -s ../Frameworks/VLCKit.framework/lib .
-# cd $POS
-sed -e "s/$APPNAME.rc/$APPNAME.icns/g" -e "s/yourcompany/Jo2003/g" $CONTENTS/Info.plist >$TMPFILE
+
+# patch Info.plist file ...
+sed -e "s/$APPNAME.rc/$APPNAME.icns/g" -e "s/yourcompany/Jo2003/g" -e "s/<\/dict>/	<key>CFBundleName<\/key>\\
+	<string>$OFFNAME<\/string>\\
+<\/dict>/g" $CONTENTS/Info.plist >$TMPFILE
 iconv -f ASCII -t UTF-8 $TMPFILE >$CONTENTS/Info.plist
 macdeployqt $APPNAME.app -verbose=0
 POS=`pwd`
@@ -32,9 +55,8 @@ rm -rf QtXmlPatterns.framework
 
 cd $POS
 mkdir -p $DSTFOLDER
-mv $APPNAME.app $DSTFOLDER/
+mv $APPNAME.app $DSTFOLDER/$OFFNAME.app
 ln -s /Applications $DSTFOLDER/
 
-# mv $APPNAME.dmg $APPNAME-2.${MINORVER}${BETAEXT}-${DATESTR}-mac.dmg
 cd ..
 
