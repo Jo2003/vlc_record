92d74cd11be5b10a87732fe4bc3ab75eaa56ae19
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2013-12-01 14:53:31 +0000

    - fixing Mac OSX Mavericks font issue

--- main.cpp
+++ main.cpp
@@ -86,6 +86,15 @@ int main(int argc, char *argv[])
    qRegisterMetaType<vlcvid::SContextAction>("vlcvid::SContextAction");
    qRegisterMetaType<QLangVector>("QLangVector");
 
+#ifdef Q_OS_MACX
+    if ( QSysInfo::MacintoshVersion > QSysInfo::MV_10_8 )
+    {
+        // fix Mac OS X 10.9 (mavericks) font issue
+        // https://bugreports.qt-project.org/browse/QTBUG-32789
+        QFont::insertSubstitution(".Lucida Grande UI", "Lucida Grande");
+    }
+#endif
+
    int          iRV = -1;
    QApplication app(argc, argv);
    Recorder    *pRec;
