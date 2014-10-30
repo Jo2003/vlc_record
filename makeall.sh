22632d38bc55d94dc272e244340dbd7a2c00d78c
Olenka.Joerg <Olenka.Joerg@4efd2022-0513-11df-958a-03df8639dc2f>
2014-10-30 08:38:58 +0000

    take care of new deb build system

--- makeall.sh
+++ makeall.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+ARCH="i386"
+
+if [ "$(uname -i)" == "x86_64" ] ; then
+	ARCH="amd64"
+fi
 
 CUSTOMS="vlc-record kartina_tv polsky_tv"
 
 for i in $CUSTOMS; do
-	rm -rf ../${i}_2.*
-	qmake $i.pro
+	sudo rm -rf ../${i}_2.*${ARCH}*
+	qmake-qt4 $i.pro
 	make clean
 	make -j4 release
 	sudo make -f install.mak
 done
-
 
