78140eb6c888c3e4629c3ef91e3c066fbff1d4f4
Jo2003 <olenka.joerg@gmail.com>
2016-11-25 16:40:19 +0100

    force only poster width, but not hight

--- ivi_kartina_credits.h
+++ ivi_kartina_credits.h
@@ -16,19 +16,26 @@
 
 #include <QtGlobal>
 
+/**
+  Please note!
+  All values in this file are dummy values! If you want to
+  use IVI video service, you have to contact them to get real
+  credentials!
+ */
+
 // operating system ...
 #ifdef Q_OS_LINUX
-    #define IVI_APP_VERSION 5508
-#elif defined Q_OS_WIN32
-    #define IVI_APP_VERSION 5510
-#elif defined Q_OS_MAC
-    #define IVI_APP_VERSION 5509
-#else
     #define IVI_APP_VERSION 1234
+#elif defined Q_OS_WIN32
+    #define IVI_APP_VERSION 2345
+#elif defined Q_OS_MAC
+    #define IVI_APP_VERSION 3456
+#else
+    #define IVI_APP_VERSION 4567
 #endif // op sys
 
-#define IVI_KEY               "bde9e570140e26daee8b4cdce3d5bb85"    ///< replace with correct data!
-#define IVI_K1                "e75d776a96bc563c"                    ///< replace with correct data!
-#define IVI_K2                "cebaeed52d78ac63"                    ///< replace with correct data!
+#define IVI_KEY               "be270e1b2ca4f211fa573f3b17b20f3f"    ///< replace with correct data!
+#define IVI_K1                "81d99183076c13ad"                    ///< replace with correct data!
+#define IVI_K2                "03b323060ed82741"                    ///< replace with correct data!
 
 #endif // __20161125_IVI_KARTINA_CREDITS_H
