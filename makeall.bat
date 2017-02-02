79103bcc72d988b13a727d792a83ff1dc2817c5f
Jo2003 <olanke.joerg@gmail.com>
2017-02-02 09:08:06 +0100

    fix windows batch build issues

--- makeall.bat
+++ makeall.bat
@@ -4,20 +4,24 @@ rem Please insert the needed pathes here ...
 set QTDIR=c:\Qt\4.8.7
 set SEVENZIP=c:\Program Files\7-Zip
 set MINGW=c:\Qt\mingw_4.8.0\mingw32
-set NSIS=c:\Program Files\NSIS
+set NSIS=c:\Program Files\NSIS\Unicode
 
 rem nothing to change below this line ...
 rem ########################################
 
 set PATH=%MINGW%\bin;%NSIS%;%QTDIR%\bin;%SEVENZIP%;%PATH%
 
-rem clean old releases ...
+:: clean old releases ...
 cd packages && del *.* && cd ..
 
-rem build all packages ...
-FOR %%B IN (tvclub) DO qmake.exe %%B.pro && make -s clean && make -s -j3 release && cd installer && makensis %%B.nsi && cd ..
-
-rem 7zip packages ...
-cd packages
-FOR %%B IN (*.exe) DO 7z a %%~nB.zip %%B
-cd ..
+:: build all packages ...
+FOR %%B IN (tvclub) DO (
+    qmake.exe %%B.pro
+    make clean
+    make -j3 release
+    cd installer
+    makensis %%B.nsi
+    cd ..\packages
+    FOR %%X IN (%%B*.exe) DO 7z a %%X.zip %%X
+    cd ..
+)
